425e2296a6492a41d85ad7cf0af6b52892267fae
Jocelyn Dupuis <jdupuis@my365.bellevue.edu>
2026-03-22 11:03:07 -0400

    Added comments to java and html files for Assignment 1.3

--- demo/src/main/java/com/bookclub/DemoApplication.java
+++ demo/src/main/java/com/bookclub/DemoApplication.java
@@ -1,3 +1,9 @@
+/*
+    Walls, C. (2019). Spring In Action (5th ed.). Shelter Island, NY: Manning Publications.
+        Modified by Terrence Galamison (2023)
+    Dupuis, J. (2025). CSI 505 Intermediate Java Programming. Bellevue University. Modified by A. Student 2025.
+*/
+
 package com.bookclub;
 
 import org.springframework.boot.SpringApplication;
